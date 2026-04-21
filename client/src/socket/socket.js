44c7cd0b4a78f9981d73e40cd56c31bfc439a015
sulabhambule <sulabhambule@gmail.com>
2026-04-21 16:39:24 +0530

    Added  URL for backend

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,17 +1,6 @@
 import { io } from "socket.io-client";
 
-// const socket = io("https://codecollab-x7b5.onrender.com/", {
-//   autoConnect: false,
-
-//   // ✅ polling first (mandatory for handshake), then websocket
-//   transports: ["polling", "websocket"],
-
-//   // ✅ REQUIRED when backend uses CORS with credentials
-//   withCredentials: true,
-// });
-
-
-const socket = io("http://localhost:5000/" || "https://codecollab-x7b5.onrender.com/", {
+const socket = io("https://codecollab-x7b5.onrender.com/", {
   autoConnect: false,
 
   // ✅ polling first (mandatory for handshake), then websocket
@@ -20,5 +9,16 @@ const socket = io("http://localhost:5000/" || "https://codecollab-x7b5.onrender.
   // ✅ REQUIRED when backend uses CORS with credentials
   withCredentials: true,
 });
+
+
+// const socket = io("http://localhost:5000/", {
+//   autoConnect: false,
+
+//   // ✅ polling first (mandatory for handshake), then websocket
+//   transports: ["polling", "websocket"],
+
+//   // ✅ REQUIRED when backend uses CORS with credentials
+//   withCredentials: true,
+// });
 
 export default socket;
